9ac1fb67fd9c67fb4547c81b81d8d097dfc5d2d7
Vatsal Dhuvad <vatsaldhuvad23@gmail.com>
2026-06-23 21:28:50 +0530

    Add files via upload

--- src/graph_builder.py
+++ src/graph_builder.py
@@ -1,1 +1,17 @@
 # Graph construction logic
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+
+df = pd.read_csv("data/feature_engineered.csv")
+
+coords = df[["lat", "long"]]
+
+knn = NearestNeighbors(n_neighbors=5)
+
+knn.fit(coords)
+
+distances, indices = knn.kneighbors(coords)
+
+print(indices[:5])
+
+print("KNN graph constructed successfully.")
